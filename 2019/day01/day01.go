80d3260fffa058381541320b251ff497410aa051
Bert Riffelmacher <sotsoguk@gmail.com>
2019-12-02 00:19:03 +0100

    Removed one temp variable

--- 2019/day01/day01.go
+++ 2019/day01/day01.go
@@ -16,22 +16,19 @@ func getFuel(mass int64) int64 {
 }
 func main() {
 	lines := readAOC.ReadInput("2019/inputs/input01_2019.txt")
+
 	var (
 		solution1, solution2 int64
 	)
 
 	for _, l := range lines {
-		var tmpFuel int64
 		lint, _ := strconv.ParseInt(l, 10, 64)
 		solution1 += getFuel(lint)
 		for lint > 0 {
 			fuel := getFuel(lint)
-			tmpFuel += fuel
+			solution2 += fuel
 			lint = fuel
-
 		}
-		solution2 += tmpFuel
-
 	}
 	fmt.Printf("AoC 2019 - Day 01\n-----------------\nLength of Input (lines):\t%v\n\nSolution:\nPart1:\t%v\nPart2:\t%v",
 		len(lines), solution1, solution2)
